6d24414b2da500da46a6198a9c83b86c4389c4bf
Mateusz Jaroszko <matt7269@protonmail.com>
2023-07-07 22:58:46 +0200

    Dodaj pliki projektów.

---
+++ Controllers/TaskController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+
+namespace Lab5.Controllers
+{
+    public class TaskController : ApiController
+    {
+        private static List<Task> tasks = new List<Task>(); // Przykładowa lista zadań
+
+        public IHttpActionResult CreateTask(Task task)
+        {
+            if (task == null)
+            {
+                return BadRequest("Nieprawidłowe dane zadania");
+            }
+
+            tasks.Add(task);
+            return Ok("Zadanie zostało utworzone");
+        }
+
+        public IHttpActionResult GetAllTasks()
+        {
+            return Ok(tasks);
+        }
+
+        public IHttpActionResult GetTaskById(int id)
+        {
+            var task = tasks.FirstOrDefault(t => t.Id == id);
+
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(task);
+        }
+        public IHttpActionResult UpdateTask(int id, Task updatedTask)
+        {
+            var task = tasks.FirstOrDefault(t => t.Id == id);
+
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            task.Title = updatedTask.Title;
+            task.Description = updatedTask.Description;
+            task.Status = updatedTask.Status;
+
+            return Ok("Zadanie zostało zaktualizowane");
+        }
+        public IHttpActionResult DeleteTask(int id)
+        {
+            var task = tasks.FirstOrDefault(t => t.Id == id);
+
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            tasks.Remove(task);
+            return Ok("Zadanie zostało usunięte");
+        }
+    }
+
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ Testy/TaskManagerTest.cs
@@ -0,0 +1,60 @@
+﻿using Lab5;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testy
+{
+    internal class TaskManagerTest
+    {
+        private TaskManager _taskManager;
+        [SetUp]
+        public void Setup()
+        {
+            _taskManager = new TaskManager();
+        }
+        [Test]
+        public void AddTask_ShouldIncreaseTaskCount()
+        {
+            // Arrange
+            var task = new Lab5.Task("Testowy task");
+            // Act
+            _taskManager.AddTask(task);
+            // Assert
+            Assert.AreEqual(1, _taskManager.GetTasks().Count);
+        }
+        [Test]
+        public void RemoveTask_ExistingTask_ShouldDecreaseTaskCount()
+        {
+            // Arrange
+            var task = new Lab5.Task("temp task");
+            _taskManager.AddTask(task);
+            // Act
+            _taskManager.RemoveTask(0);
+            // Assert
+            Assert.AreEqual(1, _taskManager.GetTasks().Count);
+            foreach ( var t in _taskManager.GetTasks() )
+            {
+                if (t.Title != "temp task")
+                {
+                    Assert.AreEqual(1, 2);
+                }
+            }
+        }
+
+        [Test]
+        public void MarkTaskAsCompleted_ExistingTask_ShouldMarkTaskAsCompleted()
+        {
+            // Arrange
+            var task = new Lab5.Task("false task");
+            _taskManager.AddTask(task);
+            // Act
+            _taskManager.MarkTaskAsCompleted(1);
+            // Assert
+            Assert.AreEqual(true, _taskManager.GetTaskById(1).IsCompleted);
+        }
+
+    }
+}
